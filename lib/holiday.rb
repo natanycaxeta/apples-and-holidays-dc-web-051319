4bc3c64aa4ae509f29d224f186cf9a30737458a5
Natany Caxeta <natanycaxeta@icloud.com>
2019-05-05 00:32:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,7 +18,9 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
-  holiday_hash
+  supply = ["Fireworks", "BBQ"]
+  
+supply.join(", ")
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
