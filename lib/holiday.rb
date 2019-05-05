7fa925d24765731bc93794ba3718b51d01a643d8
Natany Caxeta <natanycaxeta@icloud.com>
2019-05-05 01:06:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,9 +49,10 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-holiday_hash[:winter].map do |holidays, items|
-  holiday_hash(items)
-end
+  array = holiday_hash[:winter].map do |holiday, items|
+    items
+  end
+  array.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
