8ed3a5da9e034012ba856678c6998b9056067889
Natany Caxeta <natanycaxeta@icloud.com>
2019-05-05 01:05:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,6 +52,7 @@ def all_winter_holiday_supplies(holiday_hash)
 holiday_hash[:winter].map do |holidays, items|
   puts items
 end
+end
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
